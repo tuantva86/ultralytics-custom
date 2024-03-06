d077fd6d16e1d5aa848d9beb4df8864f6f198d4d
tuantva86 <126761746+tuantva86@users.noreply.github.com>
2024-03-06 16:02:06 +0700

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -71,9 +71,9 @@ class BboxLoss(nn.Module):
         """IoU loss."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
         #IoU
-        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=False, DIoU = False)
+        #iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=False, DIoU = False)
         #CIoU
-        #iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True, DIoU = False)
+        iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True, DIoU = False)
         #DIoU
         #iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False,CIoU=False, DIoU = True)
         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
